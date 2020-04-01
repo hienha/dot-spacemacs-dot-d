3b879fa9c6f4b2a8ee6ccd512124ff7bfbfe95f2
vcinent <vincent@hienha.com>
2020-04-01 10:42:20 +0800

    1. custom layer 2. improve company mode and occur-mode

--- init.el
+++ init.el
@@ -44,17 +44,15 @@ This function should only modify configuration layer settings."
      emacs-lisp
      git
      helm
-     ;; lsp
      markdown
      multiple-cursors
      org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
      spell-checking
      syntax-checking
      treemacs
-     ;; version-control
+     (chinese :variables
+              chinese-default-input-method 'wubi
+              chinese-enable-youdao-dict t)
      )
 
    ;; List of additional packages that will be installed without being
@@ -64,7 +62,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(youdao-dictionary)
+   dotspacemacs-additional-packages '()
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
