1f921ef0f7047b4db9e60869c04b1893bdd226b0
vcinent <vincent@hienha.com>
2020-04-02 16:53:51 +0800

    Configure org-agenda mode, so I can manager my GTD tasks in Spacemacs too

--- vincent/config.el
+++ vincent/config.el
@@ -1,3 +1,24 @@
+;; org agenda files
+(setq org-agenda-files '("~/org"))
+(defvar org-agenda-dir ""
+  "gtd org files location")
+
+(defvar deft-dir ""
+  "deft org files location")
+
+(defvar blog-admin-dir ""
+  "blog-admin files location")
+
+(if (spacemacs/system-is-mswindows)
+    (setq
+     org-agenda-dir "c:/Users/vincent/tasks/org/"
+     deft-dir "c:/Users/vincent/tasks/org/"
+     blog-admin-dit "c:/Users/vincent/Documents/tasks/org/")
+  (setq
+   org-agenda-dir "~/Documents/tasks/org/"
+   deft-dir "~/Documents/tasks/org/"
+   blog-admin-dit "~/Documents/workspace/goproj"))
+
 (global-linum-mode t)
 
 (global-set-key (kbd "M-s o") 'occur-dwim)
